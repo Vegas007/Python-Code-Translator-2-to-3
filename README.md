5f7b423e104a2d668d89f7cff3b66df9aa15fee9
Constantin Emanuel <dany.sem76@yahoo.com>
2021-01-22 23:44:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Python-Code-Translator-2-to-3
+
+Is a Python program that reads Python 2.x source code and applies a series of fixers to transform it into valid Python 3.x code ***that work on all versions (py2.x - py3.x).***
+The standard library contains a rich set of fixers that will handle almost all code.
+A flexible and generic library, so it is possible to write your own fixers based on your purposes.
+
+##### INSTALLATION 
+
+- https://www.python.org/downloads/release/python-391/
+
+
+##### USING
+- Add your .py scripts to modules folder.
+- Run main.bat
+- Take the output files and move into your application.
+
+##### FIXERS
+
+###### print
+- Converts the print statement to the print() function.
+- Matching multiple formats, comments, and more.
+
+###### Exception handling
+- Convert except to except BaseException, since BaseException is the base class for all built-in exceptions.
+- Converts except X, T to except X as T.
+
+###### xrange
+- Renames xrange() to range() and wraps existing range() calls with list.
+
+###### has_key
+- Changed dict.has_key(key) to dict..__contains____(key)
+
+###### dict
+- Fixes dictionary iteration methods. dict.iteritems() is converted to dict.items(), dict.iterkeys() to dict.keys(), and dict.itervalues() to dict.values(). 
+Similarly, dict.viewitems(), dict.viewkeys() and dict.viewvalues() are converted respectively to dict.items(), dict.keys() and dict.values(). 
+It also wraps existing usages of dict.items(), dict.keys(), and dict.values() in a call to list.
+
+###### exec - TODO
+- Converts the exec statement to the exec() function.
+
+###### apply - TODO
+- Removes usage of apply(). For example apply(function, *args, **kwargs) is converted to function(*args, **kwargs).
+
+###### raise - TODO
+- Converts raise E, V to raise E(V), and raise E, V, T to raise E(V).with_traceback(T). If E is a tuple, the translation will be incorrect because substituting tuples for exceptions has been removed in 3.0.
+
---
+++ main.py
@@ -0,0 +1,158 @@
+# !/usr/bin/python
+# -*- coding: utf-8 -*-
+__author__ = "VegaS"
+__date__ = "2021-01-22"
+__version__ = "1.0.1"
+
+import sys
+import os
+import re
+import typing
+
+# Check python version
+if sys.version_info < (3, 7):
+	print("This script needs at least Python 3.7.\nYou're using {}.".format('.'.join(map(str, sys.version_info[:3]))))
+	sys.exit(0)
+
+os.chdir(os.path.dirname(os.path.realpath(__file__)))
+
+# Working directory
+INPUT_DIRECTORY: str = 'modules'
+
+# Disable pycharm inspections
+DISABLE_PYCHARM_INSPECTION_TUPLE: tuple = (
+	# [...]
+)
+
+"""
+Print regex patterns
+Replace the print statement with a print() function, with keyword arguments
+to replace most of the special syntax of the old print statement.
+:examples:
+	print 100						print (100)
+	print "test {}".format(100)		print ("test {}".format(100))
+	print var #comment				print (var) #comment
+"""
+REGEX_PRINT_DICT: typing.Dict[str, str] = {
+	r'print\s+(?![\'\"])([^#\n\r]+(?<! ))?': r'print(\1)',
+	r'print\s+(ur|ru)?([\'\"]{1,3})([\w\W]+?)(\2)([^#\n\r]+(?<! ))?': r'print(\1\2\3\4\5)',
+	r'print\s+(ur|ru)?([\'\"])(?!\2)([^\r\n]+)(\2)([^#\n\r]+(?<! ))?': r'print(\1\2\3\4\5)',
+}
+
+"""
+Exception regex patterns
+Fix all exceptions for all versions of python.
+
+BaseException is the base class for all built-in exceptions.
+Handling of exceptions has slightly changed in Python3+.
+We have to use the 'as' keyword now.
+
+:examples:
+	except RuntimeError, msg:		except RuntimeError as msg:
+	except:							except BaseException:
+"""
+EXCEPT_REGEX_DICT: typing.Dict[str, str] = {
+	r'except\s+(.*?),(.*?:)': r'except \1 as \2',
+	'except:': 'except BaseException:',
+}
+
+"""
+# General regex patterns
+Fix general changes made in python3.
+	dict.has_key(), use the in operator instead.
+	range() now behaves like xrange() used to behave, except it works with values of arbitrary size. 
+	dict.iterkeys(), dict.iteritems() and dict.itervalues() methods are no longer supported.
+
+:examples:
+	for i in xrange(100)		for i in range(100
+	if dict.has_key(100)		if dict.__contains__(100)
+"""
+GENERAL_REGEX_DICT: typing.Dict[str, str] = {
+	'xrange': 'range',
+	'has_key': '__contains__',
+	'iteritems': 'items',
+	'viewitems': 'items',
+	'itervalues': 'values',
+	'viewvalues': 'values',
+	'iterkeys': 'keys',
+	'viewkeys': 'keys',
+}
+
+
+class CodeTranslatorPy2To3:
+	def __init__(self) -> typing.NoReturn:
+		self.moduleNameSet: set = set()
+
+	@staticmethod
+	def has_inspection(fileContent: str) -> bool:
+		"""
+		A method that checking if a specific disabled inspection already exists in a specific file.
+		:param fileContent: str
+		:return: bool
+		"""
+		if not DISABLE_PYCHARM_INSPECTION_TUPLE:
+			return True
+
+		for inspectionName in DISABLE_PYCHARM_INSPECTION_TUPLE:
+			if inspectionName in fileContent:
+				return True
+		return False
+
+	@staticmethod
+	def append_inspection(fileContent: str) -> str:
+		"""
+		A prepend method to insert a line to the beginning of a file.
+		:param fileContent: str
+		:return: str
+		"""
+		inspections = '\n'.join(DISABLE_PYCHARM_INSPECTION_TUPLE)
+		return f"{inspections}\n{fileContent}"
+
+	@staticmethod
+	def process_function(regexDict: typing.Dict[str, str], fileContent: str, flags: int = re.IGNORECASE) -> str:
+		"""
+		A method that processes different replacements by regex patterns.
+		:param regexDict: typing.Dict[str, str]
+		:param fileContent: str
+		:param flags: int
+		:return: str
+		"""
+		for pattern, replace in sorted(regexDict.items()):
+			fileContent = re.sub(pattern, replace, fileContent, flags=flags)
+		return fileContent
+
+	def initialize(self) -> typing.NoReturn:
+		"""
+		A method that generates the file names in a directory tree by walking the tree either top-down or bottom-up.
+		If topdown is set to False, directories are scanned from the bottom-up.
+		"""
+		for root, dirs, files in os.walk(INPUT_DIRECTORY, topdown=False):
+			for fileName in files:
+				if fileName.endswith('.py'):
+					self.moduleNameSet.add(os.path.join(root, fileName))
+
+	def process_modules(self) -> typing.NoReturn:
+		"""
+		A method that opening all modules and doing specific actions.
+		"""
+		for moduleName in self.moduleNameSet:
+			with open(moduleName, 'r+') as fileStream:
+				# Store the content of the file
+				fileContent: str = fileStream.read()
+				# Sets the file's current position at the offset, the position of the read/write pointer within the file
+				fileStream.seek(0, 0)
+				#  Truncates the file's size
+				fileStream.truncate()
+
+				# Process regex patterns
+				for regexDict in (REGEX_PRINT_DICT, EXCEPT_REGEX_DICT, GENERAL_REGEX_DICT):
+					fileContent = self.process_function(regexDict, fileContent)
+
+				# Rewrite the processed content of the file
+				fileStream.write(fileContent)
+
+
+if __name__ == '__main__':
+	instance = CodeTranslatorPy2To3()
+	instance.initialize()
+	instance.process_modules()
